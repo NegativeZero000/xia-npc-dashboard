6691dd6ad3bd3f62e0885bce8b96570618b8b399
Matt Cameron <negativezero000@gmail.com>
2025-05-08 18:25:56 -0400

    Build basic grid layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
-import './App.css'
+import { Box, Grid, GridItem, Text } from "@chakra-ui/react";
+import "./App.css";
 
 function App() {
-
-  return (
-    <div>Hello World!</div>
-  )
+    return (
+        <Grid
+            templateAreas={{
+                base: `"navigation" "main" "footer"`,
+            }}
+            templateColumns={{
+                base: "1fr",
+            }}
+        >
+            <GridItem area="navigation" padding={2}>
+                <Box backgroundColor="green.400">
+                    <Text>Navigation</Text>
+                </Box>
+            </GridItem>
+            <GridItem area="main" padding={2}>
+                <Box backgroundColor="blue.400">
+                    <Text>Main</Text>
+                </Box>
+            </GridItem>
+            <GridItem area="footer" padding={2}>
+                <Box backgroundColor="gray.400">
+                    <Text>Footer</Text>
+                </Box>
+            </GridItem>
+        </Grid>
+    );
 }
 
-export default App
+export default App;
