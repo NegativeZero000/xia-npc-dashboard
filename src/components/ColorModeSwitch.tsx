c3c638a93ed2fb6ae871fff9f032f2eacb49c799
Matt Cameron <negativezero000@gmail.com>
2025-05-09 17:25:16 -0400

    color switch formatting

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -1,26 +1,21 @@
-"use client"
+"use client";
 
-import { HStack, Switch } from "@chakra-ui/react"
-import { useColorMode } from "../components/ui/color-mode"
+import { HStack, Switch } from "@chakra-ui/react";
+import { useColorMode } from "../components/ui/color-mode";
 
 const ColorModeSwitch = () => {
-  const { toggleColorMode,colorMode } = useColorMode()
-  return (
-    <HStack>
-    <Switch.Root colorPalette='green' checked={colorMode === 'dark'} onCheckedChange={toggleColorMode}>
-    <Switch.HiddenInput />
-      <Switch.Control>
-        <Switch.Thumb />
-      </Switch.Control>
-      <Switch.Label whiteSpace="nowrap">Dark Mode</Switch.Label>
-    </Switch.Root>
+    const { toggleColorMode, colorMode } = useColorMode();
+    return (
+        <HStack>
+            <Switch.Root colorPalette="green" checked={colorMode === "dark"} onCheckedChange={toggleColorMode}>
+                <Switch.HiddenInput />
+                <Switch.Control>
+                    <Switch.Thumb />
+                </Switch.Control>
+                <Switch.Label whiteSpace="nowrap">Dark Mode</Switch.Label>
+            </Switch.Root>
+        </HStack>
+    );
+};
 
-    {/* <Button variant="outline" onClick={toggleColorMode}>
-      Toggle Mode
-    </Button> */}
-
-    </HStack>
-  )
-}
-
-export default ColorModeSwitch
+export default ColorModeSwitch;
