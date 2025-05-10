61830639b7ffc019b5ff4c5128559abf3fcbffbf
Matt Cameron <negativezero000@gmail.com>
2025-05-10 08:54:40 -0400

    added more details to ship template

--- src/components/Ships.tsx
+++ src/components/Ships.tsx
@@ -6,36 +6,52 @@ interface Props {
 }
 
 const Ships = ({ ship }: Props) => {
-    const headingSize = "sm"
+    const headingSize = "sm";
     return (
         <HStack padding={3} gap={2}>
-            <Stack textAlign="left" >
+            <Stack textAlign="left">
                 <Text fontSize={headingSize}>Name</Text>
                 <Text fontSize="large"> {ship.name}</Text>
             </Stack>
-            <Stack textAlign="left" >
+            <Stack textAlign="left">
                 <Text fontSize={headingSize}>Spawn</Text>
                 <Text fontSize="large"> {ship.spawn}</Text>
             </Stack>
-            <Stack textAlign="left" >
+            <Stack textAlign="left">
                 <Text fontSize={headingSize}>Movement</Text>
                 <Text fontSize="large"> {ship.movementRate}</Text>
             </Stack>
-            <Stack textAlign="left" >
+            <Stack textAlign="left">
                 <Text fontSize={headingSize}>Attack</Text>
-                <Text fontSize="large"> {ship.numberOfAttackDice}D{ship.attackDie} {ship.attackType}</Text>
+                <Text fontSize="large">
+                    {ship.numberOfAttackDice}D{ship.attackDie} {ship.attackType}
+                </Text>
             </Stack>
-            <Stack textAlign="left" >
+            <Stack textAlign="left">
                 <Text fontSize={headingSize}>Defence</Text>
-                <Text fontSize="large"> {ship.numberOfDefenceDice}D{ship.defenceDie} {ship.defenceType}</Text>
+                <Text fontSize="large">
+                    {ship.numberOfDefenceDice}D{ship.defenceDie} {ship.defenceType}
+                </Text>
             </Stack>
-            <Stack textAlign="left" >
+            <Stack textAlign="left">
                 <Text fontSize={headingSize}>Life Points</Text>
                 <Text fontSize="large"> {ship.lifePoints}</Text>
             </Stack>
-            <Stack textAlign="left" >
+            <Stack textAlign="left">
                 <Text fontSize={headingSize}>On Kill</Text>
-                <Text fontSize="large"> {ship.baseRewardFamePoints}FP + {ship.bounties} bounites</Text>
+                <Text fontSize="large">
+                    {ship.baseRewardFamePoints}FP + {ship.bounties} bounites
+                </Text>
+            </Stack>
+            <Stack textAlign="left">
+                <HStack>
+                    <Text fontSize="large">Target</Text>
+                    <Text fontSize="initial">{ship.targetPreference}</Text>
+                </HStack>
+                <HStack>
+                    <Text fontSize="large">Target Path</Text>
+                    <Text fontSize="initial">{ship.targetPath}</Text>
+                </HStack>
             </Stack>
         </HStack>
     );
